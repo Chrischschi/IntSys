acad7e5691beca6df1f0469ddab1f74f593ae35a
timolange <timolange@gmx.de>
2014-10-03 14:02:47 +0200

    Regel cousin hinzugefuegt, alle bisherigen Regeln getestet

--- code/stammbaum.pl
+++ code/stammbaum.pl
@@ -56,10 +56,20 @@ kind_aus(don_juan,liselotte,elizabeth).
 mutter(Mutter, Kind):-kind_aus(_,Mutter, Kind).
 vater(Vater, Kind) :- kind_aus(Vater,_, Kind).
 eltern(Kind,Eltern) :- kind_aus(Vater,Mutter,Kind),Eltern=(Vater,Mutter).
+%%   man kann nicht sein eingener bruder oder seine eigene schwester
+%%   sein, dies gilt egal ob vollgeschwister oder halbgeschwister.
 geschwister(Person1,Person2) :- vollgeschwister(Person1,Person2);halbgeschwister(Person1, Person2).
-vollgeschwister(Person1, Person2) :- kind_aus(Vater, Mutter,Person1),kind_aus(Vater,Mutter,Person2).
-halbgeschwister(Person1,Person2) :- kind_aus(Vater1, Mutter1, Person1), kind_aus(Vater2, Mutter2, Person2), Vater1\=Vater2.
-halbgeschwister(Person1,Person2) :- kind_aus(Vater1, Mutter1, Person1), kind_aus(Vater2, Mutter2, Person2), Mutter1\=Mutter2.
+
+vollgeschwister(Person1, Person2) :- kind_aus(Vater, Mutter,Person1),kind_aus(Vater,Mutter,Person2), Person1 \= Person2.
+%% Bei halbgewschwistern muss ein elternteil gleich sein und das andere
+%% darf nicht gleich sein, daher am ende die
+%% gleichheitstest
+halbgeschwister(Person1,Person2) :- kind_aus(Vater1, Mutter1, Person1), kind_aus(Vater2, Mutter2, Person2),Person1 \= Person2, Vater1\=Vater2,Mutter1=Mutter2.
+halbgeschwister(Person1,Person2) :- kind_aus(Vater1, Mutter1, Person1), kind_aus(Vater2, Mutter2, Person2),Person1 \= Person2, Mutter1\=Mutter2,Vater1 = Vater2.
+cousin(C1, C2) :- kind_aus(_V1,M1,C1), geschwister(M1,M2), kind_aus(_V2,M2,C2). %mütter sind geschwister
+cousin(C1, C2) :- kind_aus(_V1,M1,C1), geschwister(M1,V2), kind_aus(V2,_M2,C2). %Mutter der cousine ist die schwester deines Vaters
+cousin(C1, C2) :- kind_aus(V1,_M1,C1), geschwister(V1,V2), kind_aus(V2,_M2,C2). %väter sind geschwister
+cousin(C1, C2) :- kind_aus(V1,_M1,C1), geschwister(V1,M2), kind_aus(_V2,M2,C2). %Vater der cousine ist ist der bruder deiner mutter
 
 
 
