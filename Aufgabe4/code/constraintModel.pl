f8bd2686db48e72c7ef43b581fded0e06694fe1d
timolange <timolange@gmx.de>
2014-12-10 15:09:27 +0100

    Modell erster Ansatz

--- Aufgabe4/code/constraintModel.pl
+++ Aufgabe4/code/constraintModel.pl
@@ -1,1 +1,37 @@
 %Prolog
+
+%Constraints
+c1(Nationalitaet, Haustier, Getraenk, Zigarette, HausFarbe, HausPosition).
+
+%Variablen Belegungen
+
+nationalitaet(brite).
+nationalitaet(schwede).
+nationalitaet(daene).
+nationalitaet(norweger).
+nationalitaet(deutscher).
+
+haustier(hund).
+haustier(vogel).
+haustier(katze).
+haustier(fisch).
+haustier(pferd).
+
+getraenk(tee).
+getraenk(kaffee).
+getraenk(bier).
+getraenk(milch).
+getraenk(wasser).
+
+zigarette(pallMall).
+zigarette(dunhill).
+zigarette(marlboro).
+zigarette(winfield).
+zigarette(rothmanns).
+
+hausFarbe(rot).
+hausFarbe(gruen).
+hausFarbe(weiss).
+hausFarbe(gelb).
+hausFarbe(blau).
+
