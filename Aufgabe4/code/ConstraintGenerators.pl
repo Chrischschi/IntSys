4dab6c753bae6568e129b6d95aaa28ec82869635
Christian Schirin <Christian.Schirin@haw-hamburg.de>
2014-12-14 22:12:02 +0100

    Angefangen mit implementation des Top-Levels (solve_csp)

--- Aufgabe4/code/ConstraintGenerators.pl
+++ Aufgabe4/code/ConstraintGenerators.pl
@@ -3,7 +3,9 @@
 %"Factory-Methoden" für einzelne constraints, damit sich daraus eine
 % Datenstruktur daraus erzeugen lassen kann.
 % Einige Prädikate geben listen von constraints, andere nur einzelne constraints 
-% zurück
+% zurück.
+% Außerdem beinhaltet diese Datei das Prädikat elems_in_bounds/4, welche
+% Variablen zusammen mit ihren Domänen erzeugt
 
 eq(A,B,eq(A,B)).
 
@@ -18,30 +20,13 @@ all_different(Vars,Constraints) :-
   (member(Left,Vars),member(Right,Vars),Left \== Right),
   Constraints).
 
-/*
-makeDifs(Vars,Difs,LastElement) :- 
-  makeDifs(Vars,Difs,_,LastElement).
-
-makeDifs([],[],Last,Last).
-makeDifs([First,Second|Rest],[dif(First,Second)|RestDifs],Second,Last) :- 
-  makeDifs(Rest,RestDifs,Second,Last).
-makeDifs([Only|Rest],[dif(Previous,Only)|RestDifs],Previous,Last) :- 
-  makeDifs(Rest,RestDifs,Only,Last). 
-*/
 
 :- begin_tests('ConstraintGenerators').
 test(elems_in_bounds) :- 
    elems_in_bounds(1,[X,Y,Z],3,[v(X):[1,2,3],v(Y):[1,2,3],v(Z):[1,2,3]]).
 
-/*   
-test(all_different) :- 
-   all_different([X,Y,Z],[dif(X,Y),dif(Y,Z),dif(Z,X)]).
-*/
 test(all_different) :- 
    all_different([X,Y,Z],[dif(X,Y),dif(X,Z),dif(Y,X),dif(Y,Z),dif(Z,X),dif(Z,Y)]).
-/*
-test(makeDifs) :- 
-  makeDifs([X,Y,Z],[dif(X,Y),dif(Y,Z)],Z).
-*/
+
 :- end_tests('ConstraintGenerators').
 
--- Aufgabe4/code/SolverTopLevel.pl
+++ Aufgabe4/code/SolverTopLevel.pl
@@ -1,5 +1,18 @@
 % SolverToplevel.pl
+% 14.11.2014 Christian Schirin
+:- ensure_loaded('EinsteinRaetsel.pl').
 
 main :- 
+  model_einstein([Brite,Schwede,Daene,Norweger,Deutscher],Fisch,VarsAndDoms,ConstraintNet),
+  solve_csp(VarsAndDoms,ConstraintNet),
+  write(VarsAndDoms), nl, write(ConstraintNet). 
+     
+solve_csp(Variables,Constraints) :-
+  solve_csp(Variables,Variables,Constraints).
   
-
+solve_csp(RemainingVars,AllVars,Constraints) :- fail. %% TODO
+  
+   
+   
+ 
+   
