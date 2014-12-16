77d96a562cf427f2ccfbd8c28a54624b0322dee6
Chrischschi <Christian.Schirin@haw-hamburg.de>
2014-12-16 18:55:15 +0100

    Constraint-Solver vollständig implementiert, Scheint in eine endlossschleife zu laufen.
    Unsicher bei: wie AC3-LA die aktualisierten Domänen der variablen wieder an solve_csp übergibt

--- Aufgabe4/code/SolverTopLevel.pl
+++ Aufgabe4/code/SolverTopLevel.pl
@@ -1,6 +1,8 @@
 ﻿% SolverToplevel.pl
 % 14.11.2014 Christian Schirin
 :- ensure_loaded('EinsteinRaetsel.pl').
+:- ensure_loaded('ConstraintGenerators.pl').
+:- ensure_loaded('Constraints.pl').
 
 main :- 
   model_einstein([Brite,Schwede,Daene,Norweger,Deutscher],Fisch,VarsAndDoms,ConstraintNet),
@@ -11,7 +13,7 @@ main :-
 solve_csp([],_Constraints).    
 solve_csp([Var|RestVars],Constraints) :-
   %TODO: Entscheiden, ob zuerst ac3_la oder bind_var kommt
-  ac3_la(Var,RestVars,Constraints,IsConsistent), %Erst Kantenkonsistenz herstellen
+  ac3_la(Var,RestVars,Constraints,NewRestVars,IsConsistent), %Erst Kantenkonsistenz herstellen
   IsConsistent, %Prüfen, ob die Domäne irgendeiner variable leer ist
   bind_var(Var,Constraints), %Dann Variable mit konkretem Wert belegen
   solve_csp(NewRestVars,Constraints).
@@ -31,7 +33,7 @@ bind_var(Variable,_Constraints) :-
 TODO: Dafür sorgen, dass die modifizierten domänen zurückgegeben werden
     !!! */
 % 0. "procedure AC3-LA(cv)"
-ac3_la(v(Vcv):_Dcv,NextVars,Arcs,RetConsistent) :- 
+ac3_la(v(Vcv):_Dcv,NextVars,Arcs,NewNextVars,RetConsistent) :-
   % 1. "Q <- {(Vi,Vcv) in arcs(G),i>cv};" Hier: X ist Vi, Y ist Vcv
   % Das "i > cv" braucht man in dieser implementation nicht, weil stattdessen
   % geprüft wird, ob X in NextVars ist. univ muss verwendet werden, da sonst
@@ -43,21 +45,21 @@ ac3_la(v(Vcv):_Dcv,NextVars,Arcs,RetConsistent) :-
   % 2. "consistent <- true";
   Consistent = true, 
   % Iteration ausgelagert, Zeilen 4 bis 9 des 
-  ac3la_while(Q,Consistent,NextVars,UnivArcs,RetConsistent)
+  ac3la_while(Q,Consistent,NextVars,UnivArcs,NewNextVars,RetConsistent)
   % 10. "return consistent"
   . % 11. "end AC3-LA" 
 
 /* Abbruch. aus 3. "while not Q empty & consistent" wird 
    "until Q empty | not consistent" */   
-ac3la_while([],Consistent,_,_,Consistent). 
-ac3la_while(_,false,_,_,false).
+ac3la_while([],Consistent,ChangedNextVars,_,ChangedNextVars,Consistent).
+ac3la_while(_,false,ChangedNextVars,_,ChangedNextVars,false).
 
 /* Iteration. */
 
 /*  !!!
 TODO: Dafür sorgen, dass die modifizierten domänen zurückgegeben werden
     !!! */
-ac3la_while(Q,Consistent,NextVars,UnivArcs,RetConsistent) :- 
+ac3la_while(Q,Consistent,NextVars,UnivArcs,ChangedNextVars,RetConsistent) :-
   % 4. "select and delete any arc (Vk,Vm) from Q"; 
   % Statt select einfach das vorderste element aus Q nehmen. 
   % Statt delete einfach den Rest von Q nehmen. 
@@ -74,10 +76,10 @@ ac3la_while(Q,Consistent,NextVars,UnivArcs,RetConsistent) :-
     getDomain(Vk,ChangedNextVars,Dk),
     checkNotEmpty(Dk,NewConsistent),
     % 8. "endif"
-    ac3la_while(NewQ,NewConsistent,ChangedNextVars,UnivArcs,RetConsistent)
+    ac3la_while(NewQ,NewConsistent,ChangedNextVars,UnivArcs,_,RetConsistent)
   % 8a. else {Q <- Q; consistent <- consistent;} endelse 
   % if ohne else geht nicht in funktionalen/deklarativen programmiersprachen
-  ; ac3la_while(RestQ,Consistent,NextVars,UnivArcs,RetConsistent)
+  ; ac3la_while(RestQ,Consistent,NextVars,UnivArcs,_,RetConsistent)
   )
   % 9 "endwhile"
   .
@@ -86,14 +88,14 @@ ac3la_while(Q,Consistent,NextVars,UnivArcs,RetConsistent) :-
 % wenn Domain von Vi leer ist, dann fertig
 revise(Constraint,Vi,Vj,VarsAndDoms,NewVarsAndDoms,Delete):-
   getDomain(Vi,VarsAndDoms,DomI),
-  getDomain(Vj,VarsAndDoms,DomVj),
+  getDomain(Vj,VarsAndDoms,DomJ),
   reviseHelp(Constraint,DomI,DomJ,NewDomI,Delete),
   %hier neue VarsAndDoms mit der neuen Domain von Vi bauen
   %eventuell zu lists:selectchk/4 ändern (weil deterministisch)
   lists:select(v(Vi):DomI,VarsAndDoms,v(Vi):NewDomI,NewVarsAndDoms)
   .
-reviseHelp(Constraint,[],DomJ,[],false).
-reviseHelp(Constraint,[],DomJ,[],true).
+reviseHelp(_Constraint,[],_DomJ,[],false).
+reviseHelp(_Constraint,[],_DomJ,[],true).
 reviseHelp(Constraint,[HeadDomI|RestDomI],DomJ,[HeadDomI|RestNewDomI],Delete) :-
   bagof(SupportVar,(member(SupportVar,DomJ),call(Constraint,HeadDomI,SupportVar)),_SupportingVars),!,
   reviseHelp(Constraint,RestDomI,DomJ,RestNewDomI,Delete).
