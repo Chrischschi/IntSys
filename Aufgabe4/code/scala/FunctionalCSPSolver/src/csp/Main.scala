005c1c67f2269e8d36806fa0eb0a2b24a359391c
Christian Schirin <Christian.Schirin@haw-hamburg.de>
2015-01-20 15:26:09 +0100

    Schönere, verständliche ausgabe der lösung implementiert

--- Aufgabe4/code/scala/FunctionalCSPSolver/src/csp/Main.scala
+++ Aufgabe4/code/scala/FunctionalCSPSolver/src/csp/Main.scala
@@ -1,10 +1,29 @@
 package csp
 
+import csp.Solver.ConstraintNet
+
 /**
  * Created by christian on 17.01.15.
  */
 object Main {
 
+
+  def prettyPrintSolution(solvedConstraintNet: ConstraintNet): Unit = {
+    val varsOfSolution = solvedConstraintNet.variables
+
+    val symbolsAsValues = varsOfSolution groupBy {v => v._2} mapValues {l => l map {elem => elem._1 } }
+
+    val intsAsKeys = for {
+      (intList, symbols) <- symbolsAsValues
+        int <- intList
+    } yield (int,symbols)
+
+    for ((key, List(nationality, color, pet, smoke, drink)) <- intsAsKeys ) {
+      println(s"Der $nationality wohnt im ${color}en Haus mit der nummer $key, hat ein/eine/einen $pet als Haustier, " +
+        s" raucht gerne $smoke und trinkt gerne $drink.")
+    }
+
+  }
 
   def main(args: Array[String]) = {
     val einstein = EinsteinModel.model
@@ -13,6 +32,8 @@ object Main {
     val solved = Solver.solve(einstein)
 
     println(s"lösung: $solved")
+
+    prettyPrintSolution(solved._1)
   }
 
 }
